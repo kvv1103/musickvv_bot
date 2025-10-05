16617c4d97402e68f99946af3819a9a7c557a1ff
kvv1103 <kvv1103@gmail.com>
2025-10-05 10:21:17 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,37 @@
+import aiosqlite
 
+DB_NAME = "database.db"
+
+async def init_db():
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY,
+            username TEXT
+        )
+        """)
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS tracks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            file_id TEXT,
+            title TEXT,
+            FOREIGN KEY (user_id) REFERENCES users(user_id)
+        )
+        """)
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS playlists (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            name TEXT
+        )
+        """)
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS playlist_tracks (
+            playlist_id INTEGER,
+            track_id INTEGER,
+            FOREIGN KEY (playlist_id) REFERENCES playlists(id),
+            FOREIGN KEY (track_id) REFERENCES tracks(id)
+        )
+        """)
+        await db.commit()
