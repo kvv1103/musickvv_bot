d222f2c203a60b831bb615bf5ba28ac248b88202
kvv1103 <kvv1103@gmail.com>
2025-10-07 09:56:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 BOT_TOKEN = "8484157544:AAH-VnpqojEVo3_obfOCDXULZ1sGXUTIYQs"
 
 # Назва бази даних (одна єдина база для всього бота)
-DB_NAME = "music_bot.db"
+DB_NAME = "music.db"
 
 # Папка, де зберігається локальна музика
 MUSIC_FOLDER = "music"
